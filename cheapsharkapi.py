e1203694209d7f951737bb0b616d5565bc71dbd6
Russel Nicosia <russel.nicosia@charter.com>
2022-11-09 02:29:17 -0700

    minor

--- cheapsharkapi.py
+++ cheapsharkapi.py
@@ -7,7 +7,7 @@ def return_cheapest(cheapshark_response_json, store_response_init_json):
     # need to fix the below poop. Trying to check to see if the next title matches the previous title and if so ignore it.
     # it should look at the previous title and previous sale price and if the same then ignore it.
     for games_counts, games in enumerate(cheapshark_response_json):
-        return f"Title #{games_counts}: {games['title']}"
+        #return f"Title #{games_counts}: {games['title']}"
         if games['title'] == game_title_check and games['salePrice'] == game_sale_price:
             game_title_check = cheapshark_response_json[games_counts]['title']
             game_sale_check = cheapshark_response_json[games_counts]['salePrice']
