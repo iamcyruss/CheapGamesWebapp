6a747620767132ef0b8829ed4639c7e10bd4ee5a
Russ <russnicosia@gmail.com>
2022-11-14 22:31:57 -0700

    added db

--- flask_app.py
+++ flask_app.py
@@ -1,9 +1,11 @@
 import requests
 from cheapsharkapi import return_cheapest as rc
 from flask import Flask, render_template
+import os
 
 # https://apidocs.cheapshark.com/
 
+SQLPASS = os.getenv("SECRET_KEY")
 CHEAPSHARK_API_DEALS = "https://www.cheapshark.com/api/1.0/deals"
 CHEAPSHARK_API_STORES = "https://www.cheapshark.com/api/1.0/stores"
 CHEAPSHARP_REDIRECT = "https://www.cheapshark.com/redirect?dealID="
@@ -18,6 +20,15 @@ cheapshark_response.raise_for_status()
 cheapshark_response_json = cheapshark_response.json()
 app = Flask(__name__)
 app.config["DEBUG"] = True
+SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
+    username="rnicosia",
+    password=SQLPASS,
+    hostname="rnicosia.mysql.pythonanywhere-services.com",
+    databasename="rnicosia$cheapgames",
+)
+app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
+app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 @app.route('/', methods=["GET", "POST"])
 def index():
