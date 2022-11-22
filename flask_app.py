47e2a8e027fa3bcf7bd85352a363b93682db720e
Russ <russnicosia@gmail.com>
2022-11-22 09:37:44 -0500

    trying something new

--- flask_app.py
+++ flask_app.py
@@ -1,11 +1,14 @@
 import requests
 from cheapsharkapi import return_cheapest as rc
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request, url_for
 import os
 
 # https://apidocs.cheapshark.com/
 
 SQLPASS = os.getenv("SQLPASS")
+page_number = 10
+lower_price = 0
+upper_price = 50
 CHEAPSHARK_API_DEALS = "https://www.cheapshark.com/api/1.0/deals"
 CHEAPSHARK_API_STORES = "https://www.cheapshark.com/api/1.0/stores"
 CHEAPSHARP_REDIRECT = "https://www.cheapshark.com/redirect?dealID="
@@ -13,7 +16,10 @@ store_response_init = requests.get(url=CHEAPSHARK_API_STORES)
 store_response_init.raise_for_status()
 store_response_init_json = store_response_init.json()
 deals_params = {
-    "sortBy": "Price"
+    "sortBy": "Price",
+    "pageNumber": page_number,
+    "lowerPrice": lower_price,
+    "upperPrice": upper_price,
 }
 cheapshark_response = requests.get(url=CHEAPSHARK_API_DEALS, params=deals_params)
 cheapshark_response.raise_for_status()
@@ -34,10 +40,15 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 data = rc(cheapshark_response_json, store_response_init_json)
 
+user_input_data = []
+
 @app.route('/', methods=["GET", "POST"])
 def index():
-    return render_template("main_page.html", cheapshark_data=data)
+    if request.method == "GET":
+        return render_template("main_page.html", cheapshark_data=data, user_input_data=user_input_data)
 
+    user_input_data.append(request.form["contents"])
+    return redirect(url_for('index'))
 
 @app.route('/wibble')
 def wibble():
