97523a9081cb430c0c13b23c254937b7803ae766
iamcyruss <russnicosia@gmail.com>
2022-12-21 07:46:01 -0700

    adding price alerts

--- cheapsharkapi.py
+++ cheapsharkapi.py
@@ -81,9 +81,12 @@ def set_alert(CHEAPSHARK_API_ALERT, alert_params):
 
 def manage_alerts(CHEAPSHARK_API_ALERT, alert_params):
     manage_alert = requests.get(url=CHEAPSHARK_API_ALERT, params=alert_params)
+    return manage_alert
+    """
     if manage_alert.status_code == 200:
         all_good = 'Email Sent!'
         return all_good
     else:
         no_good = 'Something went wrong. Please try again.'
         return no_good
+    """
