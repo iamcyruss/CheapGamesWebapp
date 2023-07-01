c3dcf9985efac75529d465c3e60dbf92a3d0cb03
iamcyruss <russnicosia@gmail.com>
2023-07-01 15:02:39 -0600

    added gpt

--- flask_app.py
+++ flask_app.py
@@ -200,5 +200,5 @@ def submit():
     return render_template('ask_gpt.html', answer=answer)
 
 
-if __name__ == '__flask_app__':
-    app.run(debug=False)
+if __name__ == '__main__':
+    app.run(debug=True)
